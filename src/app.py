b3d3e405e9f5f8fc9e815aed5e923d206bec0145
kiranokaze <kirkronag@icloud.com>
2026-01-05 13:56:26 +0400

    upd

---
+++ src/app.py
@@ -0,0 +1,18 @@
+import os
+import time
+import sys
+
+def clear_screen():
+    os.system("clear")
+
+def show_menu():
+    if os.path.exists("menu.txt")
+        with open("menu.txt", "r") as file:
+            print(file.read())
+
+def main():
+
+    clear_screen()
+
+    show_menu()
+
