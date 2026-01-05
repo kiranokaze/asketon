43378d15b341e2be9b6b217d5966077ac23e7380
kiranokaze <kirkronag@icloud.com>
2026-01-05 21:13:32 +0000

    upd

--- src/app.py
+++ src/app.py
@@ -1,22 +1,102 @@
-import os
 import time
+import tty
+import termios
 import sys
+import subprocess
 
 def clear_screen():
-    os.system("clear")
+    
+    sys.stdout.write("\033[H\033[2J")
+    sys.stdout.flush()
 
-def show_menu():
-    if os.path.exists("menu.txt"):
-        with open("menu.txt", "r") as file:
-            print(file.read())
+def spinner():
+    
+    spinner = ["| ", "/ ", "- ", "\\ "]
+    for spin in spinner:
+        clear_screen()
+        print(spin, end="", flush=True)
+        time.sleep(0.1)
+        clear_screen()
+        
+def show_menu(menu_lines):
 
+    for i, line in enumerate(menu_lines):
+		
+        if i == len(menu_lines) - 1:
+            print(line, end="", flush=True)
+            
+        else:
+            print(line)
+            time.sleep(0.015) 
+
+def get_char():
+    
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        char = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return char
+
+def about():
+    
+    for i in range(10):
+        print("")
+    print("     https://github.com/kiranokaze ", end="", flush=True)
+        
+    get_char()
+    
+def git():
+    
+    result = subprocess.run(
+        ["git", "status"],
+        text=True,
+        capture_output=True
+    )
+    print(result.stdout)
+    
+    print("\n[home] [p]-push [l]-pull ", end="", flush=True)
+    choice = get_char().lower()
+
+    if choice == "p":
+        subprocess.run(["git", "add", "."])
+        subprocess.run(["git", "commit", "-m", "upd"])
+        subprocess.run(["git", "push"])
+        
+    elif choice == "l":
+        subprocess.run(["git", "pull"])
+        
 def main():
+    
+    try:
+        with open("menu.txt", "r", encoding="utf-8") as f:
+            menu_lines = f.read().splitlines()
+    except FileNotFoundError:
+        menu_lines = ["no file."]
+    
+    while True:
 
-    clear_screen()
+        clear_screen()
 
-    show_menu()
+        show_menu(menu_lines)
 
-    choice = input("enter the first letter.. ").strip().lower()
+        choice = get_char().lower()
+		
+        clear_screen()
+        
+        
+        if choice == "a":
+            about()
+            
+        elif choice == "g":
+            git()
+            
+        elif choice == "q":
+            for i in range(3):
+                spinner()
+            break
 
 if __name__ == "__main__":
     main()
