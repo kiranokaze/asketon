f01013fccbdef5c898900df6b84e0a464210f7be
kiranokaze <kirkronag@icloud.com>
2026-01-05 14:14:16 +0400

    upd

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ def clear_screen():
     os.system("clear")
 
 def show_menu():
-    if os.path.exists("menu.txt")
+    if os.path.exists("menu.txt"):
         with open("menu.txt", "r") as file:
             print(file.read())
 
@@ -15,4 +15,9 @@ def main():
     clear_screen()
 
     show_menu()
+
+    choice = input("enter the first letter.. ").strip().lower()
+
+if __name__ == "__main__":
+    main()
 
