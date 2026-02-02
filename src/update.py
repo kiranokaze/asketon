930c67919509f755abd4962e60c8403791aa55d6
kiranokaze <kirkronag@icloud.com>
2026-02-02 19:04:56 +0000

    upd

--- src/update.py
+++ src/update.py
@@ -4,12 +4,14 @@ import time
 
 
 def update():
-	print("| ",end="", flush=True)
+	print("fetch ",end="", flush=True)
     
 	try:
 		git.git_fetch()
         
 		if git.has_remote_commits():
+			clear_last_line()
+			print("pull ",end="", flush=True)
 			git.git_pull()
             
 	except Exception:
