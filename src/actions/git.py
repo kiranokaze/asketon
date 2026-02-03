e1528c595e620e7b4ecb13f6887afd261ff2fdfb
kiranokaze <kirkronag@icloud.com>
2026-02-03 11:39:57 +0000

    upd

--- src/actions/git.py
+++ src/actions/git.py
@@ -44,7 +44,7 @@ def git_push():
         return
         
     clear_last_line()
-    print("push ", end="", flush=True)
+    print("push", end="", flush=True)
     
     subprocess.run(["git", "add", "."])
     subprocess.run(
