98aaedb56a7e600082e191181687363f7bcdc838
te-nk <kirkronag@icloud.com>
2026-02-07 17:23:57 +0000

    upd

--- src/actions/sync.py
+++ src/actions/sync.py
@@ -6,21 +6,19 @@ import socket
 import os
 
 def fix_network():
-    print("[checking network...]", end=" ", flush=True)
+    print("checking network...", end=" ", flush=True)
     try:
         socket.gethostbyname('github.com')
-        print("0K")
+        print("OK")
     except socket.gaierror:
         print("DNS FAIL")
-        print("[patching /etc/resolv.conf...]", end="")
+        print("patching /etc/resolv.conf...", end="")
         try:
             with open('/etc/resolv.conf', 'w') as f:
                 f.write("nameserver 8.8.8.8\nnameserver 1.1.1.1\n")
             print("DONE")
         except PermissionError:
             print("FAILED (no root)")
-
-def git_fetch():
     
     subprocess.run(["git", "fetch"])
     
@@ -33,7 +31,7 @@ def git_status():
     )
     print(result.stdout)
     
-    print("\n[home]  [p]-push [d]-pull ", end="", flush=True)
+    print("[q] [p]-push [d]-pull ", end="", flush=True)
 
 def git_push():
 
@@ -41,20 +39,19 @@ def git_push():
     subprocess.run(["git", "commit", "-m", "upd"])
     subprocess.run(["git", "push"])
     
-    print("\n[home] ", end="", flush=True)
+    print("\n[q] ", end="", flush=True)
     read_key()
     
 def git_pull():
     
     subprocess.run(["git", "pull"])
     
-    print("\n[home] ", end="", flush=True)
+    print("\n[q] ", end="", flush=True)
     read_key()
 
 def run():
     
     fix_network()
-    git_fetch()
     git_status()
     
     choice = read_key().lower()
